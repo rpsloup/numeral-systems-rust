1130888f94de280dd8cbc17376c97888cee77947
rpsloup <robin.patrik.sloup@proton.me>
2022-11-18 20:32:45 +0100

    Loading inputs for binary and hexadecimal

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,30 @@
 #[allow(unused)]
 
 use std::io;
+use std::io::{Write};
 use rand::Rng;
 
 fn main() {
-    loop {
-        let mut user_input: String = String::new();
-        let mut rng = rand::thread_rng();
-        let random_num = rng.gen_range(0..=10);
+    let mut bin_input: String = String::new();
+    let mut hex_input: String = String::new();
+    let mut rng = rand::thread_rng();
 
-        println!("{}", random_num);
+    loop {
+        let random_num = rng.gen_range(1..=255);
+
+        println!("DEC: {}", random_num);
+        print!("BIN: ");
+        io::stdout().flush().expect("Flush failed!");
         io::stdin()
-            .read_line(&mut user_input)
-            .expect("Invalid input.");
+            .read_line(&mut bin_input)
+            .expect("Invalid input!");
+        
+        print!("HEX: ");
+        io::stdout().flush().expect("Flush failed!");
+        io::stdin()
+            .read_line(&mut hex_input)
+            .expect("Invalid input!");
+
+        println!();
     }
 }
- 
