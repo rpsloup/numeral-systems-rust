ac8a6fa5bcf30451cb5c8d4e9bbc67de45037de4
rpsloup <robin.patrik.sloup@proton.me>
2022-11-18 20:22:26 +0100

    Generating random numbers

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
 #[allow(unused)]
 
+use std::io;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut user_input: String = String::new();
+        let mut rng = rand::thread_rng();
+        let random_num = rng.gen_range(0..=10);
+
+        println!("{}", random_num);
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Invalid input.");
+    }
 }
+ 
